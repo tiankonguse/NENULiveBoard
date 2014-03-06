47f5f165bb1b7182cd9e2d6d74eacd43c5a4ffd5
tiankonguse <i@tiankonguse.com>
2014-03-06 15:23:00 +0800

    update

---
+++ WebContent/js/_boardinfo.js
@@ -0,0 +1,953 @@
+var cs_boardinfo = {
+	"message" : "Information fetched successfully.",
+	"teams" : {
+		"team1" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "袁小康",
+				"en" : "tiankonguse",
+				"m" : true
+			} ],
+			"coach" : {
+				"cn" : "tiankonguse",
+				"en" : "tiankonguse",
+				"m" : true
+			},
+			"cn" : "tiankonguse",
+			"en" : "team1",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team2" : {
+			"sid" : "_2",
+			"teamMembers" : [ {
+				"cn" : "team2",
+				"en" : "team2",
+				"m" : true
+			} ],
+			"cn" : "someone",
+			"coach" : {
+				"cn" : "team2",
+				"en" : "team2",
+				"m" : true
+			},
+			"en" : "team2",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team3" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team3",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team3",
+			"coach" : {
+				"cn" : "team3",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team3",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team4" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team4",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team4",
+			"coach" : {
+				"cn" : "team4",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team4",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team5" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team5",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team5",
+			"coach" : {
+				"cn" : "team5",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team5",
+			"girlsTeam" : false,
+			"offical" : true
+		}
+		,
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		},
+		"team6" : {
+			"sid" : "_1",
+			"teamMembers" : [ {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			} ],
+			"cn" : "team6",
+			"coach" : {
+				"cn" : "team6",
+				"en" : "Yaobo Liang",
+				"m" : true
+			},
+			"en" : "team6",
+			"girlsTeam" : false,
+			"offical" : true
+		}
+
+	},
+	"schools" : {
+		"_1" : {
+			"cn" : "东北师范大学 ~ 计算机学院",
+			"en" : "Northeast Normal University CS"
+		},
+		"_2" : {
+			"cn" : "东北师范大学 ～ 软件学院",
+			"en" : "Northeast Normal University SW"
+		}
+	},
+	"code" : 0
+};
